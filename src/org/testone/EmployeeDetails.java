d3c5878f694d8635bfca64bef5010c0b80d20a72
anandgitacc <anand.akmail@gmail.com>
2022-03-09 15:35:10 +0530

    sivagit

--- src/org/testone/EmployeeDetails.java
+++ src/org/testone/EmployeeDetails.java
@@ -13,6 +13,12 @@ public class EmployeeDetails {
 			
 			System.out.println("Employee Mobile : 8939278836");
 		}
+		
+		private void employeeStatus() {
+			
+			System.out.println("Working");
+		}
+		
 		//               main method
 		public static void main(String[] args) {
 			
